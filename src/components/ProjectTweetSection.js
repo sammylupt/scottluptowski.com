63db44fe08f9fc9579bce88619bbbf3822a4ed9f
Scott Luptowski <scottluptowski@gmail.com>
2017-01-05 22:10:10 -0500

    Moving tweet section to own file

---
+++ src/components/ProjectTweetSection.js
@@ -0,0 +1,33 @@
+// @flow
+
+import React, { Component } from 'react'
+import TweetEmbed from 'react-tweet-embed'
+
+class ProjectTweetSection extends Component {
+
+  props: {
+    tweets: Array<string>
+  }
+
+  render() {
+    const { tweets } = this.props
+
+    return (
+      <div>
+        { tweets.map((tweet: string, i: number) => {
+          return (
+            <div className="tweet-wrapper" key={i}>
+              <TweetEmbed id={tweet} key={i} preview={
+                <div className="tweet-loading">Loading...</div>
+              } 
+              />
+            </div>
+          )
+          })
+        }
+      </div>
+    )
+  }
+}
+
+export default ProjectTweetSection
--- src/pages/ProjectPage.js
+++ src/pages/ProjectPage.js
@@ -2,10 +2,10 @@
 
 import React, { Component } from 'react'
 import ReactDocumentTitle from 'react-document-title'
-import TweetEmbed from 'react-tweet-embed'
 
 import ProjectImageList from '../components/ProjectImageList'
 import ProjectLinkSection from '../components/ProjectLinkSection'
+import ProjectTweetSection from '../components/ProjectTweetSection'
 import type { Project } from '../types'
 
 class ProjectPage extends Component {
@@ -21,6 +21,7 @@ class ProjectPage extends Component {
         <ReactDocumentTitle
           title={pageTitle}
         />
+
         <p className="project-header">{name}</p>
         <p className="project-summary"
            dangerouslySetInnerHTML={{__html: summary}}
@@ -33,16 +34,7 @@ class ProjectPage extends Component {
         }
 
         { tweets &&
-          tweets.map((tweet: string, i: number) => {
-            return (
-              <div className="tweet-wrapper" key={i}>
-                <TweetEmbed id={tweet} key={i} preview={
-                  <div className="tweet-loading">Loading...</div>
-                } 
-                />
-              </div>
-            )
-          })
+          <ProjectTweetSection tweets={tweets} />
         }
       </div>
     )
