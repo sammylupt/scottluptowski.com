7e1a915eb1ef6e35eaf5e62a5a70fd9158fa0471
Scott Luptowski <scottluptowski@gmail.com>
2017-01-07 22:01:18 -0500

    Tweak indentation

--- src/components/ProjectImageList.js
+++ src/components/ProjectImageList.js
@@ -16,24 +16,23 @@ class ProjectImageList extends Component {
 
     return (
       <div className="project-images">
-      {
-        images.map((image: ImageWithMeta, i: number) => {
-          const { description, ...rest } = image
+        {
+          images.map((image: ImageWithMeta, i: number) => {
+            const { description, ...rest } = image
 
-          return (
-            <div className="project-image" key={i}>
-              <Image
-                {...rest}
-              />
-              { image.description &&
-                <div className="project-image-description">
-                  {description}
-                </div>
-              }
-            </div>
-          )
-        })
-      }
+            return (
+              <div className="project-image" key={i}>
+                <Image {...rest} />
+
+                { image.description &&
+                  <div className="project-image-description">
+                    {description}
+                  </div>
+                }
+              </div>
+            )
+          })
+        }
       </div>
     )
   }
